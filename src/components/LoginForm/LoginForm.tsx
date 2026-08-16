5c26537fd6600657d3487350a9d7c323ff742e24
Gobinath <mgobinath30@gmail.com>
2026-08-16 08:51:15 +0530

    dynamic style

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -1,41 +1,49 @@
 import LoginHeader from "./LoginHeader";
 import classes from "./LoginForm.module.css";
 import Input from "./Input";
+import { useState } from "react";
 
 export default function LoginForm() {
-  let isNameValid = false;
-  let isPwdValid = false;
+  const [isNameValid, setisNameValid] = useState<boolean>(false);
+  const [isPwdValid, setisPwdValid] = useState<boolean>(false);
+
+  //   let isNameValid = false;
+  //   let isPwdValid = false;
 
   const handleInputValidation = (
     field: string,
     event: React.ChangeEvent<HTMLInputElement>,
   ) => {
     if (field === "username") {
-      isNameValid = event.target.value !== "" && event.target.value.length >= 3;
+      const isNameValid =
+        event.target.value !== "" && event.target.value.length >= 3;
+      setisNameValid(isNameValid);
     }
     if (field === "password") {
-      isPwdValid =
+      const isPwdValid =
         event.target.value !== "" &&
-        (event.target.value.match(/[A-Za-z0-9]/g) || []).length > 3;
+        (event.target.value.match(/[A-Za-z0-9]/g) || []).length >= 3;
+      setisPwdValid(isPwdValid);
     }
-    console.log(isNameValid, isPwdValid);
   };
   return (
     <div className={classes.container}>
       <LoginHeader />
-      <div className={classes.loginform}>
+      <div
+        className={`${classes.loginform} ${isNameValid && isPwdValid ? classes.success : undefined}`}
+      >
         <Input
           label="username"
           type="text"
           name="username"
-          style={{ color: isNameValid ? "" : "red" }}
+          style={{ color: !isNameValid ? "red" : undefined }}
           onChange={(event) => handleInputValidation("username", event)}
         />
         <Input
           label="password"
           type="password"
           name="password"
-          style={{ color: isPwdValid ? "" : "red" }}
+          style={{ color: !isPwdValid ? "red" : undefined }}
           onChange={(event) => handleInputValidation("password", event)}
         />
         <button>Login</button>
